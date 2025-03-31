de99f02f877f85d37354ad507be3111a6b8b8a21
Zoras <68259064+ZorasWu@users.noreply.github.com>
2025-03-31 16:40:31 +0800

    Add files via upload

--- A_07_02.asm
+++ A_07_02.asm
@@ -1,10 +1,3 @@
-; Title: ICP-A07-02 = Transmit a numeral ASCII after receiving it from serial input (at 19200 bps)- template
-; Note: When you run this program, set the system clock at 11.059 MHz and baud rate at 19200 bps.
-; Note: In this template, the content of the ISR "serialPortISR" and the subroutine "numeralTest" ae empty.
-;       You need to program these two parts.
-; Note: When this program runs, serial tansmission and receiving can happen simulaneously.
-;       Hence, the ISR "serialPortISR" needs to be able to handle the two serial interrupt flags TI and RI.
-
 ORG 0
 	JMP main 
 ORG 0023H 			  ; serial port interrupt vector
@@ -23,60 +16,48 @@ main:
 	SETB REN ; receiver enable - This bit must be set to enable data receiving.
 	SETB ES ; enable serial port interrupts
 	SETB EA ; global interrupt enable
+;MOV SBUF, #'A'  ; 發送 'A'
 	JMP $   ; do nothing but waiting for interrupt
 
 ; ################################
-; Please program the ISR "serialPortISR" below.
-; --------------------------------
+
 SERIAL_ISR:
-    JNB RI, check_TI  ; 檢查 RI (Receive Interrupt)，如果沒資料就跳過
+    JNB RI, check_TI  ; 如果 RI = 0，則跳過
+    
+    MOV R0, SBUF       ; **讀取收到的資料**
+    CLR RI            ; **清除 RI 讓它能繼續接收**
+    CALL numeralTest  ; **判斷是否為數字**
+    JNC ignore        ; 如果 C = 0，忽略
 
-    MOV A, SBUF       ; 讀取接收的 ASCII 字元
-    CALL numeralTest  ; 判斷是否為數字 (結果存在 C)
-   ; JNC ignore        ; 如果 C=0 (不是數字)，就忽略
-
-    MOV SBUF, A       ; 如果是數字，就傳回該字元
-    JNB TI, endISR   ; 確保 TI 清除 (若有需要)
-
-check_TI:
-    JNB TI, endISR   ; 如果 TI = 0，直接結束
-    CLR TI            ; 清除 TI 標誌 (準備好發送新資料)
-
-endISR:
-    RETI              ; 返回主程式
+    MOV SBUF, R0       ; **回傳數字**
+    SETB TI           ; **手動設置 TI**
 
 ignore:
-    CLR RI            ; 清除 RI
-    RETI
+    RETI              ; 返回主程式
 
+check_TI:
+    JNB TI, end_ISR   ; 如果 TI = 0，則結束 ISR
+    CLR TI            ; **清除 TI**
+    
+end_ISR:
+    RETI              ; 結束中斷
 
 ; ################################
-; [ICP-A07-01
-; Subroutine name: numeralTest
-; Learning object: To learn the encoding of ASCII code
-; Function: 
-;      This subroutine tests whether the accumulator contains the ASCII code
-;      of a numeral symbol (i.e., 0, 1, 2, ..., or 9). 
-; Input: Acc = the code to be tested.
-; Output: C = 1, if '0' <= Acc <= '9' and
-;         C = 0, otherwise.
-;                      
 
 numeralTest:
+	;ljmp notNumeral
     CLR C           ; 預設 C = 0
-    MOV  B, #39H
-    MOV  R1, A      ; 備份 A
-    MOV  A, B
-    SUBB A, R1      ; 計算 39H - A
-    JC   notNumeral ; 如果 A > 39H，則跳出，C 保持 0
+	MOV A, #39H
+	MOV B, R0
+	SUBB A,B    ;   (39H-R0)>=0 ? 0 : 1
+    JC   notNumeral ; 如果 A > 3AH，則跳出，C 保持 0
 
-    MOV  B, #30H
-	MOV A,R1
-    SUBB A, B      ; 計算 30H - A
+	MOV A, R0
+	MOV B, #30H
+	SUBB A,B   ;  (R0-30H)>=0 ? 0 : 1
     JC   notNumeral ; 如果 A < 30H，則跳出，C 保持 0
 
     SETB C          ; 如果 30H <= A <= 39H，則設 C = 1
-    MOV P1.0, C
 	RET
 
 notNumeral:
